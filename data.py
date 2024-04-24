0f89bcda84e7b78313a151c07fb5819967ff2a73
changchiyou <changchiyou@gmail.com>
2024-04-24 20:18:47 +0800

    refactor: remove web scraping, extract `bases` setting( and merge into `stats.yml`) from `data.py`

--- data.py
+++ data.py
@@ -1,12 +1,8 @@
-import re
-import os
-
-import bs4
-import requests
-from bs4 import BeautifulSoup
-from yaml import dump, load
 import argparse
 import logging
+import os
+
+from yaml import dump, load
 
 try:
     from yaml import CDumper as Dumper
@@ -16,183 +12,64 @@ except ImportError:
 
 
 class ItemData:
-    def __init__(self, data_file_name: str | None = None):
-        self.main_url = "https://wr-meta.com/items/"
-        self.stat_names = [
-            "Attack Damage",
-            "Ability Power",
-            "Armor",
-            "Magic Resistance",
-            "Max Health",
-            "% Critical Rate",
-            "% Attack Speed",
-            "Move Speed",
-            "% Physical Vamp",
-            "Armor Penetration",
-            "% Armor Penetration",
-            "Magic Penetration",
-            "% Magic Penetration",
-            "Ability Haste",
-            "% Move Speed",
-            "Max Mana",
-            "% Omnivamp",
-            "% Health Regen",
-            "% Mana Regeneration",
-        ]
-        self.bases = {
-            "first_base": {
-                "Attack Damage": "Long Sword",
-                "Ability Power": "Amplifying Tome",
-                "Armor": "Cloth Armor",
-                "Magic Resistance": "Null-Magic Mantle",
-                "Max Health": "Ruby Crystal",
-                "% Critical Rate": "Brawler's Gloves",
-                "% Attack Speed": "Dagger",
-                "Move Speed": "Boots of Speed",
-                "Ability Haste": "Ring of Revelation",
-            },
-            "second_base": {
-                "% Physical Vamp": "Vampiric Scepter",
-                "Armor Penetration": "Serrated Dirk",
-                "% Armor Penetration": "Last Whisper",
-                "Magic Penetration": "Prophet's Pendant",
-                "% Move Speed": "Aether Wisp",
-                "Max Mana": "Sapphire Crystal",
-            }
-        }
-        self.datas = []
+    def __init__(
+        self, items_file_name: str | None = None, stats_file_name: str | None = None
+    ):
         self.stat_price = {}
-        self.data_file_name = data_file_name or "items.yml"
+        self.stats_file_name = stats_file_name or "stats.yml"
+        self.items_file_name = items_file_name or "items.yml"
 
-    def parse_stat(self, stat: str) -> tuple[str, int] | None:
-        try:
-            stat = stat.split("+")[1]
-        except Exception:
-            return None
-        stat = stat.strip(". ")
+        self.datas = self.read_yaml(self.items_file_name)
+        self.bases = self.read_bases()
+        print(self.bases)
 
-        for stat_name in self.stat_names:
-            if match := re.search(r"^([\d-]*) *" + stat_name + "$", stat):
-                if "-" in (value := match.group(1)):
-                    return stat_name, int(value.split("-")[1])
-                return stat_name, int(match.group(1))
-
-    def get_abs_filename(self) -> str:
+    def get_abs_filename(self, file_name: str) -> str:
         script_dir = os.path.dirname(os.path.realpath(__file__))
-        file_path = os.path.join(script_dir, f"./_data/{self.data_file_name}")
+        file_path = os.path.join(script_dir, f"./_data/{file_name}")
 
         return file_path
 
-    def write_yaml(self):
-        logging.info(f"write `datas` into {self.data_file_name}")
-        file_path = self.get_abs_filename()
+    def write_yaml(self, file_name: str):
+        logging.info(f"write `datas` into {file_name}")
+        file_path = self.get_abs_filename(file_name)
 
         with open(file_path, "w") as f:
             dump(self.datas, f, Dumper=Dumper)
 
-    def read_yaml(self):
-        logging.info(f"read `datas` from {self.data_file_name}")
-        file_path = self.get_abs_filename()
+    def read_yaml(self, file_name: str):
+        logging.info(f"read `datas` from {file_name}")
+        file_path = self.get_abs_filename(file_name)
 
-        with open(file_path, 'r') as f:
+        with open(file_path, "r") as f:
             return load(f, Loader=Loader)
-
-    def scraping(self):
-        logging.info("extract item data from WR Meta via web scraping")
-        try:
-            logging.info("request WR Meta item page")
-            response = requests.get(f"{self.main_url}", timeout=10)
-            soup = BeautifulSoup(response.text, "html.parser")
-
-            items = soup.find_all(class_="bild-img-short")
-
-            logging.info("parse with bs4 and store into `datas`")
-            for item in items:
-                image = item.find(class_="xfieldimage lazy-loaded itemimage")
-                desc = item.find("p")
-
-                parent = item.find_parent("div", class_="clearfix sect-cont-incat")
-                category = parent.find("div", class_="sect-title").text.upper()
-
-                image_url = self.main_url + image["data-src"]
-                item_name = desc.find(class_="iname").text.strip()
-
-                # base stats
-                istats: list[tuple[str, str]] = [
-                    ("", state.text) for state in desc.find_all(class_="istats")
-                ]
-                # base stats in passives
-                istats2: list[tuple[str, str]] = []
-                for stat in desc.find_all(class_="istats2"):
-                    if (
-                        stat.find_next_sibling().name == "i"
-                        and stat.find_next_sibling().children
-                        and not isinstance(
-                            list(stat.find_next_sibling().children)[0],
-                            bs4.element.NavigableString,
-                        )
-                        and list(stat.find_next_sibling().children)[0].has_attr("class")
-                        and list(stat.find_next_sibling().children)[0].get("class")
-                        == ["fr-fic", "fr-dii"]
-                    ):
-                        next_sibling_text = stat.find_next_sibling().text
-                    else:
-                        next_sibling_text = str(stat.next_element.next_element)
-
-                    if next_sibling_text and (
-                        re.fullmatch(r"^(?:&nbsp;|\xa0|\s)\+.*", next_sibling_text)
-                    ):
-                        istats2.append((stat.text.strip(":"), next_sibling_text))
-
-                istats += istats2
-
-                cost = int(desc.find(class_="goldt").text.strip())
-
-                stats = []
-                for passive, stat in istats:
-                    stat = self.parse_stat(stat)
-                    if stat:
-                        if passive:
-                            stats.append(
-                                {"type": stat[0], "value": stat[1], "passive": passive}
-                            )
-                        else:
-                            stats.append({"type": stat[0], "value": stat[1]})
-
-                self.datas.append(
-                    {
-                        "name": item_name,
-                        "image": image_url,
-                        "cost": cost,
-                        "stats": stats,
-                        "category": category,
-                    }
-                )
-
-            logging.info("write `datas` into yml")
-            self.write_yaml()
-
-        except requests.exceptions.ConnectTimeout as execinfo:
-            print(str(execinfo))
 
     def find_data_with_name(self, datas, name: str):
         for data in datas:
-            if data['name'] == name:
+            if data["name"] == name:
                 return data
 
-    def find_stat_with_type(self, data, type: str):
-        for stat in data['stats']:
-            if stat['type'] == type:
+    def find_stat_with_type(self, data, _type: str):
+        for stat in data["stats"]:
+            if stat["type"] == _type:
                 return stat
+
+    def read_bases(self):
+        stats = self.read_yaml(self.stats_file_name)
+        bases = {"first": {}, "second": {}}
+
+        for stat in stats:
+            if "base_type" in stats[stat]:
+                bases[stats[stat]["base_type"]][stat] = stats[stat]["base_item"]
+
+        return bases
 
     def calculate_base_statistic_prices(self):
         logging.info("calculate base statistic prices")
-        if self.datas:
-            datas = self.datas
-        else:
-            datas = self.read_yaml()
-        first_base = self.bases['first_base']
-        second_base = self.bases['second_base']
+
+        datas = self.datas
+
+        first_base = self.bases["first"]
+        second_base = self.bases["second"]
 
         logging.info("  - first base")
         for _type in first_base:
@@ -202,10 +79,14 @@ class ItemData:
             target_stat = self.find_stat_with_type(data, _type)
             if not (data and target_stat):
                 raise ValueError(f"data: {data}, target_stat: {target_stat}")
-            cost = data['cost']
+            cost = data["cost"]
 
-            stat_cost = float("{:.2f}".format(cost / target_stat['value']))
-            data['first_base'] = {'type': _type, 'value': stat_cost, 'formula': f"{data['cost']}/{target_stat['value']}={stat_cost}"}
+            stat_cost = float("{:.2f}".format(cost / target_stat["value"]))
+            data["first_base"] = {
+                "type": _type,
+                "value": stat_cost,
+                "formula": f"{data['cost']}/{target_stat['value']}={stat_cost}",
+            }
             self.stat_price[_type] = stat_cost
 
         logging.info("  - second base")
@@ -216,73 +97,130 @@ class ItemData:
             target_stat = self.find_stat_with_type(data, _type)
             if not (data and target_stat):
                 raise ValueError(f"data: {data}, target_stat: {target_stat}")
-            cost = data['cost']
+            cost = data["cost"]
 
             formula_minus_part = ""
-            for stat in data['stats']:
-                if stat['type'] != _type:
-                    cost -= stat['value']*self.stat_price[stat['type']]
-                    formula_minus_part += f"-{stat['value']}*{self.stat_price[stat['type']]}"
+            for stat in data["stats"]:
+                if stat["type"] != _type:
+                    cost -= stat["value"] * self.stat_price[stat["type"]]
+                    formula_minus_part += (
+                        f"-{stat['value']}*{self.stat_price[stat['type']]}"
+                    )
 
-            stat_cost = float("{:.2f}".format(cost / target_stat['value']))
-            data['second_base'] = {
-                'type': _type,
-                'value': stat_cost,
-                'formula': f"({data['cost']}{formula_minus_part})/{target_stat['value']}={stat_cost}"
+            stat_cost = float("{:.2f}".format(cost / target_stat["value"]))
+            data["second_base"] = {
+                "type": _type,
+                "value": stat_cost,
+                "formula": f"({data['cost']}{formula_minus_part})/{target_stat['value']}={stat_cost}",
             }
             self.stat_price[_type] = stat_cost
 
         self.datas = datas
         logging.info("write `datas` into yml")
-        self.write_yaml()
+        self.write_yaml(self.items_file_name)
 
     def calculate_gold_efficiency(self):
         logging.info("calculate gold efficiency based on base statistic prices")
-        if self.datas:
-            datas = self.datas
-        else:
-            datas = self.read_yaml()
+
+        datas = self.datas
 
         if not self.stat_price:
             for data in datas:
-                if 'first_base' in data:
-                    self.stat_price[data['first_base']['type']] = data['first_base']['value']
-                if 'second_base' in data:
-                    self.stat_price[data['second_base']['type']] = data['second_base']['value']
+                if "first_base" in data:
+                    self.stat_price[data["first_base"]["type"]] = data["first_base"][
+                        "value"
+                    ]
+                if "second_base" in data:
+                    self.stat_price[data["second_base"]["type"]] = data["second_base"][
+                        "value"
+                    ]
         stat_price = self.stat_price
 
         for data in datas:
-            if not ('first_base' in data or 'second_base' in data):
+            if not ("first_base" in data or "second_base" in data):
                 worth = 0
                 formula_bonux_part = ""
-                for stat in data['stats']:
-                    worth += stat['value']*stat_price.get(stat['type'], 0)
+                for stat in data["stats"]:
+                    worth += stat["value"] * stat_price.get(stat["type"], 0)
                     formula_bonux_part += f"{'+' if formula_bonux_part else ''}{stat['value']}*{stat_price.get(stat['type'], 0)}"
-                amount = "{:.2f}".format(worth/data['cost']*100) + "%"
-                data['amount'] = amount
-                data['formula'] = f"({formula_bonux_part if formula_bonux_part else '0'})/{data['cost']}={amount}"
+                amount = "{:.2f}".format(worth / data["cost"] * 100) + "%"
+                data["amount"] = amount
+                data["formula"] = (
+                    f"({formula_bonux_part if formula_bonux_part else '0'})/{data['cost']}={amount}"
+                )
             else:
-                data['amount'] = "100%"
-                data['formula'] = "Base Item"
+                data["amount"] = "100%"
+                data["formula"] = "Base Item"
 
         self.datas = datas
-        self.write_yaml()
+        self.write_yaml(self.items_file_name)
+
+    def clean_base_GE(self):
+        logging.info("Clean gold efficiency and base statistic price from item data")
+        if self.datas:
+            datas = self.datas
+        else:
+            datas = self.read_yaml(self.items_file_name)
+
+        cleared_datas = []
+
+        for data in datas:
+            new_data = dict()
+            for key in ("category", "cost", "image", "name", "stats"):
+                new_data[key] = data[key]
+            cleared_datas.append(new_data)
+
+        self.datas = cleared_datas
+        self.write_yaml(self.items_file_name)
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Extract data from WR Meta via web scraping, or calculate the gold efficiency of item data.')
-    parser.add_argument('-f', '--data_file_name', help="The file storage item data")
-    parser.add_argument('-s', '--scraping', action="store_true", help="Extract data from WR Meta via web scraping (Due to missing or incorrect information on the source website, it is advised not to use this feature)")
-    parser.add_argument('-c', '--calculate', action="store_true", help="Calculate the gold efficiency of item data")
-    parser.add_argument('-d', '--debug', action='store_true', help="Enable debug mode")
+    parser = argparse.ArgumentParser(
+        description="Extract data from WR Meta via web scraping, or calculate the gold efficiency of item data."
+    )
+    parser.add_argument("--items_file_name", help="The file storage item data")
+    parser.add_argument(
+        "-c",
+        "--calculate",
+        action="store_true",
+        help="Calculate the gold efficiency of item data",
+    )
+    parser.add_argument(
+        "--clean_only",
+        action="store_true",
+        help="Only clean gold efficiency and base statistic price from item data",
+    )
+    parser.add_argument("-d", "--debug", action="store_true", help="Enable debug mode")
 
     args = parser.parse_args()
 
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG if args.debug else logging.INFO)
 
-    item_data = ItemData(data_file_name=args.data_file_name)
-    if args.scraping is True:
-        item_data.scraping()
-    if args.calculate is True:
-        item_data.calculate_base_statistic_prices()
-        item_data.calculate_gold_efficiency()
+    item_data = ItemData(items_file_name=args.items_file_name)
+
+    if args.clean_only is True:
+        item_data.clean_base_GE()
+    else:
+        if args.calculate is True:
+            item_data.clean_base_GE()
+            item_data.calculate_base_statistic_prices()
+            item_data.calculate_gold_efficiency()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
