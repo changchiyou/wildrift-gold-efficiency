89542ac72ea5fe1ac62889bda8748566aa7ef780
changchiyou <changchiyou@gmail.com>
2024-05-13 17:33:17 +0800

    feat: comparison fold/unfolding animation

--- assets/js/renderTable.js
+++ assets/js/renderTable.js
@@ -1,11 +1,13 @@
 function renderRowColors() {
-    var rows = document.querySelectorAll("tr:not([style*='display: none'])");
+    var rows = document.querySelectorAll("tr:not(.compare):not([style*='display: none'])");
 
     rows.forEach(function(row, index) {
-        if (index % 2 === 0) {
-            row.style.backgroundColor = "#2c314294";    // $grey-color-light
-        }else{
-            row.style.backgroundColor = "#1f222e";      // $background-color
+        if (!row.classList.contains('active')) {
+            if (index % 2 === 0) {
+                row.style.backgroundColor = "#2c314294";    // $grey-color-light
+            }else{
+                row.style.backgroundColor = "#1f222e";      // $background-color
+            }
         }
     });
 }
