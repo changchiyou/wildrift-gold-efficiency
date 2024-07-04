db6356e231ca3497c23f65c3250e9bb39805d507
changchiyou <changchiyou@gmail.com>
2024-07-04 22:57:38 +0800

    feat: add index for each row
    https://github.com/users/changchiyou/projects/1/views/1\?pane\=issue\&itemId\=69744455

--- assets/js/renderTable.js
+++ assets/js/renderTable.js
@@ -1,13 +1,36 @@
 function renderRowColors() {
-    var rows = document.querySelectorAll("tr:not(.compare):not([style*='display: none'])");
+    var tables = document.querySelectorAll("table");
 
-    rows.forEach(function(row, index) {
-        if (!row.classList.contains('active')) {
-            if (index % 2 === 0) {
-                row.style.backgroundColor = "#2c314294";    // $grey-color-light
-            }else{
-                row.style.backgroundColor = "#1f222e";      // $background-color
+    tables.forEach(function(table) {
+        var rows = table.querySelectorAll("tr:not(.compare):not([style*='display: none'])");
+
+        rows.forEach(function(row, index) {
+            if (!row.classList.contains('active')) {
+                if (index % 2 === 0) {
+                    row.style.backgroundColor = "#2c314294";    // $grey-color-light
+                } else {
+                    row.style.backgroundColor = "#1f222e";      // $background-color
+                }
             }
-        }
+        });
+    });
+}
+
+
+function renderRowIndexs() {
+    var tables = document.querySelectorAll("table");
+
+    tables.forEach(function(table) {
+        var rows = table.querySelectorAll("tr:not(.compare):not([style*='display: none'])");
+
+        rows.forEach(function(row, index) {
+            if (!row.classList.contains('active')) {
+                var itemIndex = row.querySelector("p.item-index");
+                if (itemIndex) {
+                    itemIndex.style.display = 'block';
+                    itemIndex.textContent = '#' + index;
+                }
+            }
+        });
     });
 }
