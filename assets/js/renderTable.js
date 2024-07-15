5a921540305cc6de2bacb5db29d5947cec0ee3f7
changchiyou <changchiyou@gmail.com>
2024-07-15 15:51:43 +0800

    enhance: show index only if items are sorted by Cost / Amount column
    https://github.com/changchiyou/wildrift-gold-efficiency/issues/19

--- assets/js/renderTable.js
+++ assets/js/renderTable.js
@@ -23,12 +23,34 @@ function renderRowIndexs() {
     tables.forEach(function(table) {
         var rows = table.querySelectorAll("tr:not(.compare):not([style*='display: none'])");
 
+        var data_order = 'non-order'
+        var costAmountCols = Array.from(table.querySelectorAll("th")).filter(function(th) {
+            return th.textContent.trim() === 'Cost' || th.textContent.trim() === 'Amount';
+        });
+        costAmountCols.forEach(function(col) {
+            if (col.getAttribute('data-order') === 'ascending') {
+                data_order = 'ascending'
+            }
+            if (col.getAttribute('data-order') === 'descending') {
+                data_order = 'descending'
+            }
+        });
+
         rows.forEach(function(row, index) {
             if (!row.classList.contains('active')) {
                 var itemIndex = row.querySelector("p.item-index");
                 if (itemIndex) {
-                    itemIndex.style.display = 'block';
-                    itemIndex.textContent = '#' + index;
+                    if (data_order === 'non-order') {
+                        itemIndex.style.display = 'none';
+                    }
+                    if (data_order === 'ascending') {
+                        itemIndex.style.display = 'block';
+                        itemIndex.textContent = '#' + (rows.length - 1 - index);
+                    }
+                    if (data_order === 'descending') {
+                        itemIndex.style.display = 'block';
+                        itemIndex.textContent = '#' + index;
+                    }
                 }
             }
         });
